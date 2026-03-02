a405ddeb9c2ed3e4c9fca0952cbf88d8387d33ef
PURAMASWANI <puramaswani10@gmail.com>
2026-03-02 14:55:01 +0530

    Add files via upload

---
+++ Jenkins/src/JenkinsGit/Hello.java
@@ -0,0 +1,7 @@
+package JenkinsGit;
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins Integration Success!");
+    }
+}
---
+++ Jenkins/src/JenkinsGit/Jenkins2.java
@@ -0,0 +1,9 @@
+package JenkinsGit;
+
+public class Jenkins2 {
+	public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+
